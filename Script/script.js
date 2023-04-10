87cc5be265fcfe524da7bac53241f673d32ee63d
salwa <salwamadihah12@gmail.com>
2023-04-10 21:35:46 +0700

    tambah

--- Script/script.js
+++ Script/script.js
@@ -13,3 +13,42 @@
 //     })
 // }
 // upIconHandler()
+
+// Navbar
+var navbar = document.getElementById("myNavbar");
+function showMenu(){
+  navbar.style.right = "0";
+}
+function hideMenu(){
+  navbar.style.right = "-400px";
+}
+    
+function fungsiDropdown() {
+  document.getElementById("dropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.test')) {
+    var dropdowns = document.getElementsByClassName("drop");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+// Carousel
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  dots: true,
+  items: 1,
+})
+
