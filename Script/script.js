6535803b11ed42a15c9174de38d985479e9a8b11
Reza <cibaduyut2@gmail.com>
2023-04-10 16:09:17 +0700

    perbaikan

---
+++ Script/script.js
@@ -0,0 +1,15 @@
+// const upIcon = document.getElementsByClassName("upIcon")[0]
+
+// function upIconHandler() {
+//     window.addEventListener("scroll", () => {
+//         let windowHeight = document.documentElement
+//         var top = (window.pageYOffset || windowHeight.scrollTop)- (windowHeight.clientTop || 0)
+//         console.log(top)
+//         if (windowHeight < 500) {
+//             upIcon.style.display = "none"
+//         } else {
+//             upIcon.style.display = "block"
+//         }
+//     })
+// }
+// upIconHandler()
